e4e8baa36b937ae576b79ae25e791490e27f2bc7
r-tavakoli <42064303+r-tavakoli@users.noreply.github.com>
2022-12-10 11:20:18 +0330

    first commit

---
+++ path_list/style.py
@@ -0,0 +1,60 @@
+LIST_BUTTON_STYLE = '''
+    QPushButton {
+        height: 20px;
+        width: 70px;
+        font-size: 14px;
+        font-family: 'Fixedsys';
+        transition: all .5s ease;
+        color: #0f0f0f;
+        border: 1px solid #22577A;
+        text-transform: uppercase;
+        text-align: center;
+        background-color: transparent;
+        padding: 5px;
+        outline: none;
+        border-radius: 4px;
+    }
+    QPushButton:hover {
+        color: #001F3F;
+        background-color: #93B5C6;
+    }
+'''
+
+CLEAR_BUTTON_STYLE = '''
+    QPushButton {
+        height: 20px;
+        width: 70px;
+        font-size: 14px;
+        font-family: 'Fixedsys';
+        transition: all .5s ease;
+        color: #0f0f0f;
+        border: 1px solid #22577A;
+        text-transform: uppercase;
+        text-align: center;
+        background-color: transparent;
+        padding: 5px;
+        outline: none;
+        border-radius: 4px;
+    }
+    QPushButton:hover {
+        color: #001F3F;
+        background-color: #F96666;
+    }
+'''
+
+LINE_EDIT_STYLE = '''
+    QLineEdit {
+        font-size: 18px;
+        font-family: 'Fixedsys';  
+        line-height: 1;
+        height: 30px;
+        border: 1px solid #22577A;
+        border-radius: 4px;
+    }
+'''
+
+TREE_VIEW_STYLE = '''
+    QTreeView:item:alternate {
+        background-color: #F5F5F5;
+    }    
+'''

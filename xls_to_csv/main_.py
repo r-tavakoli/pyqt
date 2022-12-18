8d9374d1e9f0cf617bd60fcf13bd20df2d99cdf6
r-tavakoli <42064303+r-tavakoli@users.noreply.github.com>
2022-12-18 10:04:54 +0330

    edit code format

--- xls_to_csv/main_.py
+++ xls_to_csv/main_.py
@@ -1,24 +1,28 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QDesktopWidget, QHBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QDesktopWidget, QHBoxLayout, QFormLayout
 from PyQt5.QtWidgets import (
-    QFileSystemModel,
-    QTreeView,
+    QLabel,
     QVBoxLayout,
     QLineEdit,
-    QPushButton
+    QPushButton,
+    QProgressBar
 )
+from PyQt5.QtCore import Qt, QTimer, QSize
 import style
 from pathlib import Path
+from openpyxl import load_workbook
+import csv
+
 
 class MainWindow(QWidget):
-    height = 500
-    width = 700
+    height = 200
+    width = 600
 
     def __init__(self):
         super().__init__()
 
         # windows properties
         self.setGeometry(100, 100, MainWindow.width, MainWindow.height)
-        self.setWindowTitle('Path Lister')
+        self.setWindowTitle('Excel to CSV')
 
         # fixing the size
         self.setMaximumSize(MainWindow.width, MainWindow.height)
@@ -38,33 +42,82 @@ class MainWindow(QWidget):
     def setup_ui(self):
         # layout
         main_layout = QVBoxLayout()
-        path_layout = QHBoxLayout()
-        main_layout.addLayout(path_layout)
+        form_layout = QFormLayout()
+        progress_layout = QHBoxLayout()
+        main_layout.addLayout(form_layout)
+        main_layout.addLayout(progress_layout)
 
         # widgets
+        self.excel_path_txt = QLineEdit()
+        self.excel_path_txt.setPlaceholderText(r'D:\temp\excel_file_name.xlsx')
+        self.csv_path_txt = QLineEdit()
+        self.csv_path_txt.setPlaceholderText(r'D:\temp')
 
+        self.convert_btn = QPushButton('Convert')
+        self.convert_btn.clicked.connect(self.convert)
 
-        self.tree = QTreeView()
-        self.tree.setModel(self.model)
+        self.output_lbl = QLabel('sheet out of sheets')
+
+        self.progress_bar = QProgressBar()
+        self.progress_bar.setTextVisible(False)
 
         # add widget to layout
-        path_layout.addWidget(self.path_txt)
-        path_layout.addWidget(self.clear_btn)
-        path_layout.addWidget(self.list_btn)
-        main_layout.addWidget(self.tree)
+        form_layout.addRow('Excel File Path: ', self.excel_path_txt)
+        form_layout.addRow('Save to: ', self.csv_path_txt)
+        form_layout.addRow('', self.convert_btn)
+        progress_layout.addWidget(self.output_lbl)
+        progress_layout.addWidget(self.progress_bar)
+
+        self.setLayout(main_layout)
+
+    def convert(self):
+        count_progress_bar = 0
+
+        # get path
+        source_path = self.excel_path_txt.text().encode('unicode_escape').decode().replace('\\\\', '\\')
+        destination_path = self.csv_path_txt.text().encode('unicode_escape').decode().replace('\\\\', '\\')
+        print(source_path)
+        print(destination_path)
+
+        # load workbook
+        wb = load_workbook(filename=source_path, data_only=True)
+        sheets = wb.sheetnames
+        sheets_count = len(sheets)
+
+        # set progress bar range
+        self.progress_bar.setMaximum(sheets_count)
+
+        for sheet_name in sheets:
+            # activate sheets
+            wb.active = wb[sheet_name]
+
+            # read data and save as csv
+            path = Path(destination_path, sheet_name).with_suffix('.csv')
+            with open(path, 'w', newline='') as f:
+                csv_writer = csv.writer(f)
+
+                for row in wb.active.rows:
+                    data = [cell.value for cell in row]
+                    csv_writer.writerow(data)
+
+            # update progress bar
+            count_progress_bar += 1
+            self.output_lbl.setText(f'{count_progress_bar} out of {sheets_count}')
+            self.progress_bar.setValue(count_progress_bar)
 
     def set_style(self):
         # text box
-        self.path_txt.setStyleSheet(style.LINE_EDIT_STYLE)
-
-        # tree
-        self.tree.setStyleSheet(style.TREE_VIEW_STYLE)
-        self.tree.setColumnWidth(0, 300)
-        self.tree.setAlternatingRowColors(True)
+        self.excel_path_txt.setStyleSheet(style.LINE_EDIT_STYLE)
+        self.csv_path_txt.setStyleSheet(style.LINE_EDIT_STYLE)
 
         # button
-        self.list_btn.setStyleSheet(style.LIST_BUTTON_STYLE)
-        self.clear_btn.setStyleSheet(style.CLEAR_BUTTON_STYLE)
+        self.convert_btn.setStyleSheet(style.CONVERT_BUTTON_STYLE)
+
+        # label
+        self.output_lbl.setStyleSheet(style.LABEL_STYLE)
+
+        # progress bar
+        self.progress_bar.setStyleSheet(style.progress_bar_style)
 
 
 def main():
@@ -75,4 +128,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
--- xls_to_csv/style.py
+++ xls_to_csv/style.py
@@ -1,4 +1,4 @@
-LIST_BUTTON_STYLE = '''
+CONVERT_BUTTON_STYLE = '''
     QPushButton {
         height: 20px;
         width: 70px;
@@ -20,28 +20,6 @@ LIST_BUTTON_STYLE = '''
     }
 '''
 
-CLEAR_BUTTON_STYLE = '''
-    QPushButton {
-        height: 20px;
-        width: 70px;
-        font-size: 14px;
-        font-family: 'Fixedsys';
-        transition: all .5s ease;
-        color: #0f0f0f;
-        border: 1px solid #22577A;
-        text-transform: uppercase;
-        text-align: center;
-        background-color: transparent;
-        padding: 5px;
-        outline: none;
-        border-radius: 4px;
-    }
-    QPushButton:hover {
-        color: #001F3F;
-        background-color: #F96666;
-    }
-'''
-
 LINE_EDIT_STYLE = '''
     QLineEdit {
         font-size: 18px;
@@ -53,8 +31,22 @@ LINE_EDIT_STYLE = '''
     }
 '''
 
-TREE_VIEW_STYLE = '''
-    QTreeView:item:alternate {
-        background-color: #F5F5F5;
+LABEL_STYLE = '''
+    QLabel {
+        font-size: 14px;
+        font-family: 'Fixedsys';
     }    
+'''
+
+# progress bar style
+progress_bar_style = '''
+    QProgressBar {
+        border: solid grey;
+        min-height: 15px;
+        max-height: 15px;
+    }
+    QProgressBar::chunk {
+        background-color: #05B8CC;
+        width: 1px
+    }
 '''
